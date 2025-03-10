42ed5130e39f8835c12ba1b29b2799ecde83008b
Jorge Senjudo <95231207+EndYou01@users.noreply.github.com>
2025-03-10 03:46:55 -0400

    Subiendo el tiempo del reloj

--- src/components/CountDownTimer.tsx
+++ src/components/CountDownTimer.tsx
@@ -6,7 +6,7 @@ import Countdown from 'react-countdown';
 //   const endDate = new Date().getTime() + 10000; // 10 segundos desde el momento actual
   return (
     <div className="flex justify-center items-center p-4 z-10">
-     <Countdown date={Date.now() + 30000} className='text-white z-10' onComplete={()=> {
+     <Countdown date={Date.now() + 40000} className='text-white z-10' onComplete={()=> {
       onCompleteFunction()
      }}/>
     </div>
